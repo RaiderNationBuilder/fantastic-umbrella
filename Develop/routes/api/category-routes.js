44a0083e50886e52d0427815c649237779892925
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-11-28 21:28:53 -0600

    category routes finished

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
   // be sure to include its associated Products
   Category.findAll({}).then(function(data) {
     res.json(data)
+    console.log(data)
   })
 });
 
@@ -22,19 +23,65 @@ router.get('/:id', (req, res) => {
 })
 .then(function(category) {
     res.json(category)
+    console.log(data)
   })
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "There was no category found with this id." });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'There was no category found with this id.' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
